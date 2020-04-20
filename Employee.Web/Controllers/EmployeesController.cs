606c76092836c3d7036c4c72aacfd719e252f74a
haiber bedoya <haiefra@hotmail.com>
2020-04-20 01:10:42 -0500

    first registration in employees

---
+++ Employee.Web/Controllers/EmployeesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Employee.Web.Data;
+using Employee.Web.Data.Entities;
+
+namespace Employee.Web.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public EmployeesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Employees.ToListAsync());
+        }
+
+        // GET: Employees/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employeeEntity = await _context.Employees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employeeEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(employeeEntity);
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Document,FirstName,LastName,FixedPhone,CellPhone,Address")] EmployeeEntity employeeEntity)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employeeEntity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employeeEntity);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employeeEntity = await _context.Employees.FindAsync(id);
+            if (employeeEntity == null)
+            {
+                return NotFound();
+            }
+            return View(employeeEntity);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Document,FirstName,LastName,FixedPhone,CellPhone,Address")] EmployeeEntity employeeEntity)
+        {
+            if (id != employeeEntity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employeeEntity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeEntityExists(employeeEntity.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employeeEntity);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employeeEntity = await _context.Employees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employeeEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(employeeEntity);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var employeeEntity = await _context.Employees.FindAsync(id);
+            _context.Employees.Remove(employeeEntity);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeEntityExists(int id)
+        {
+            return _context.Employees.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Employee.Web/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Employee.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee.Web.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<EmployeeEntity> Employees { get; set; }
+
+    }
+}
